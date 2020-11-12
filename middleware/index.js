1c7fb1357cb045ee1fcb8acff1d1f95b500a0128
aymfx <1095731371@qq.com>
2020-11-12 17:15:58 +0800

    init

--- middleware/index.js
+++ middleware/index.js
@@ -1,24 +1,20 @@
 const compose = require("koa-compose");
 const koaBody = require("koa-body");
 const views = require("koa-views");
+const logger = require("koa-logger");
+const koaStatic = require("koa-static");
 const path = require("path");
 
-async function responseTime(ctx, next) {
-  const start = new Date();
-  await next();
-  const ms = new Date() - start;
-  ctx.set("X-Response-Time", ms + "ms");
-  await ctx.render('index')
-}
 
 const all = [
+  logger(),
+  koaStatic(path.resolve(__dirname, "../public")),
   views(path.join(__dirname, "../views"), {
     extension: "ejs",
   }),
   koaBody({
     jsonLimit: "1kb",
   }),
-  responseTime,
 ];
 
 module.exports = compose(all);
